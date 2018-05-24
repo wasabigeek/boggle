0cfe2e7406dad40fda983ca2e5aa5614fc41c24d
Nicholas Koh <nkohwl@gmail.com>
2018-05-24 13:58:01 +0800

    start on check_word with recursive function

--- app.py
+++ app.py
@@ -14,3 +14,25 @@ def show_board():
 
 
 show_board()
+
+
+def check_word(word):
+    board = get_board()
+    # find start of word
+    possible_starts = list(filter(lambda x: x[1] in [word[0], "*"], board))
+    for start in possible_starts:
+        has_next_letter(board, start)
+
+
+def has_next_letter(board, tile):
+    adjacent_indexes = [
+        tile[0] - 4,  # top
+        tile[0] + 4,  # bottom
+        tile[0] - 1,  # left
+        tile[0] + 1,  # right
+    ]
+    adjacent_tiles = [board[i] for i in adjacent_indexes if i >= 0 and i < 16]
+    print(adjacent_tiles)
+
+
+check_word("TAPE")
