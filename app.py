58519853f93544668b604c0ea46886962bda7371
Nicholas Koh <nkohwl@gmail.com>
2018-05-24 15:17:44 +0800

    basic handling of diagonals

--- app.py
+++ app.py
@@ -33,15 +33,30 @@ def has_next_letter(board, tile, remaining_word):
     if len(remaining_word) == 0:
         return True
 
-    adjacent_indexes = [
-        tile[0] - 4,  # top
-        tile[0] + 4,  # bottom
-        tile[0] - 1,  # left
-        tile[0] + 1,  # right
-    ]
+    # find the indexes of adjacent tiles
+    row = int(tile[0] / 4)
+    col = tile[0] % 4
+
+    adjacent_indexes = []
+    if row > 0:
+        adjacent_indexes.append(tile[0] - 4)  # top
+    if row < 3:
+        adjacent_indexes.append(tile[0] + 4)  # bottom
+    if col > 0:
+        adjacent_indexes.append(tile[0] - 1)  # left
+    if col < 3:
+        adjacent_indexes.append(tile[0] + 1)  # right
+    if row > 0 and col > 0:
+        adjacent_indexes.append(tile[0] - 5)  # top-left
+    if row > 0 and col < 3:
+        adjacent_indexes.append(tile[0] - 3)  # top-right
+    if row < 3 and col > 0:
+        adjacent_indexes.append(tile[0] + 3)  # bottom-left
+    if row < 3 and col < 3:
+        adjacent_indexes.append(tile[0] + 5)  # bottom-right
+
     for i in adjacent_indexes:
-        adjacent = board[i] if (i >= 0 and i < 16) else None
-        print(f"checking {i} tile {adjacent} in {remaining_word}")
+        adjacent = board[i]
         if adjacent and (adjacent[1] in remaining_word[0] or adjacent[1] == "*"):
             return has_next_letter(board, board[i], remaining_word[1:])
 
