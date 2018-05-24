deac4371859172b0900eeff5bfd22956e6524f6a
Nicholas Koh <nkohwl@gmail.com>
2018-05-24 12:05:32 +0800

    extract get_board for use in other functions

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-def show_board():
-    with open('TestBoard.txt', 'r') as f:
+def get_board(path='TestBoard.txt'):
+    with open(path, 'r') as f:
         # convert board to a list of tiles
         board = f.read().split(', ')
-        # print out tiles in rows of 4
-        for index, tile in enumerate(board, 1):
-            # new row on every fourth tile
-            end = "\n" if index % 4 == 0 else " "
-            print(tile, end=end)
+        return list(enumerate(board))
+
+
+def show_board():
+    board = get_board()
+    for index, letter in board:
+        # new row on every fourth tile
+        end = "\n" if (index + 1) % 4 == 0 else " "
+        print(letter, end=end)
+
+
+show_board()
